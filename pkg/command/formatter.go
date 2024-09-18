8b7fb10813e695dd51e5e224c7200bcec37697e2
Mark Benjamin <mark@render.com>
2024-09-18 10:10:29 -0400

    Move initialization and add output to context

--- pkg/command/formatter.go
+++ pkg/command/formatter.go
@@ -7,6 +7,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type Output string
+
+const (
+	Interactive Output = "interactive"
+	JSON        Output = "json"
+	YAML        Output = "yaml"
+)
+
+func StringToOutput(s string) (Output, error) {
+	switch s {
+	case "json":
+		return JSON, nil
+	case "yaml":
+		return YAML, nil
+	case "interactive":
+		return Interactive, nil
+	default:
+		return "", fmt.Errorf("invalid output format: %s", s)
+	}
+}
+
 func CommandName(cmd *cobra.Command, args []string, flags map[string]string) string {
 	var flagString string
 	for k, v := range flags {
