0a7b766cd9d776947a9da26593556c061e1593b0
jakemalachowski <jake@render.com>
2024-10-09 15:27:22 -0500

    Allow user to select a service when calling ssh without a service ID

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -1,16 +1,18 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
 	"context"
 	"fmt"
+	"github.com/renderinc/render-cli/pkg/environment"
+	"github.com/renderinc/render-cli/pkg/pointers"
+	"github.com/renderinc/render-cli/pkg/project"
 	"os/exec"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/evertras/bubble-table/table"
 	"github.com/renderinc/render-cli/pkg/client"
 	"github.com/renderinc/render-cli/pkg/command"
+	"github.com/renderinc/render-cli/pkg/resource"
 	"github.com/renderinc/render-cli/pkg/service"
 	"github.com/renderinc/render-cli/pkg/tui"
 	"github.com/spf13/cobra"
@@ -19,18 +21,20 @@ import (
 // sshCmd represents the ssh command
 var sshCmd = &cobra.Command{
 	Use:   "ssh [serviceID]",
-	Args: cobra.ExactArgs(1),
+	Args:  cobra.MaximumNArgs(1),
 	Short: "SSH into a server",
-	Long:  `SSH into a server given a service ID.`,
+	Long:  `SSH into a server given a service ID. Optionally pass the service id as an argument.`,
 }
+
 var InteractiveSSH = command.Wrap(sshCmd, loadDataSSH, renderSSH)
+var InteractiveSSHSelectService = command.Wrap(sshCmd, listServices, renderSSHSelection)
 
 type SSHInput struct {
 	ServiceID string
 }
 
-func (p SSHInput) String() []string {
-	return []string{p.ServiceID}
+func (s SSHInput) String() []string {
+	return []string{s.ServiceID}
 }
 
 func loadDataSSH(ctx context.Context, in SSHInput) (string, error) {
@@ -71,15 +75,59 @@ func renderSSH(ctx context.Context, loadData func(in SSHInput) (string, error), 
 	return tui.NewExecModel(exec.Command("ssh", sshAddress)), nil
 }
 
+func listServices(ctx context.Context, _ SSHInput) ([]*service.Model, error) {
+	c, err := client.NewDefaultClient()
+	if err != nil {
+		return nil, err
+	}
+
+	serviceRepo := service.NewRepo(c)
+	environmentRepo := environment.NewRepo(c)
+	projectRepo := project.NewRepo(c)
+
+	serviceService := service.NewService(serviceRepo, environmentRepo, projectRepo)
+
+	return serviceService.ListServices(ctx, &client.ListServicesParams{
+		Type:          &[]client.ServiceType{client.WebService, client.PrivateService, client.BackgroundWorker},
+		Limit:         pointers.From(100),
+	})
+}
+
+func renderSSHSelection(ctx context.Context, loadData func(in SSHInput) ([]*service.Model, error), input SSHInput) (tea.Model, error) {
+	services, err := loadData(SSHInput{})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(services) == 0 {
+		return tui.NewSimpleModel(func() (string, error) {
+			return "No services found", nil
+		}), nil
+	}
+
+	var resources []resource.Resource
+	for _, s := range services {
+		resources = append(resources, s)
+	}
+	rows := resource.RowsForResources(resources)
+
+	return tui.NewTable(resource.ColumnsForResources(), rows, func(data []table.Row) tea.Cmd {
+		return InteractiveSSH(ctx, SSHInput{ServiceID: data[0].Data["ID"].(string)})
+	}), nil
+}
+
 func init() {
 	rootCmd.AddCommand(sshCmd)
 
 	sshCmd.RunE = func(cmd *cobra.Command, args []string) error {
 		ctx := cmd.Context()
-		serviceID := args[0]
+		if len(args) == 1 {
+			serviceID := args[0]
+			InteractiveSSH(ctx, SSHInput{ServiceID: serviceID})
+			return nil
+		}
 
-		InteractiveSSH(ctx, SSHInput{ServiceID: serviceID})
-
+		InteractiveSSHSelectService(ctx, SSHInput{})
 		return nil
 	}
 }
