9753ef4b6575ba7eaca115c81e613703e18cea7f
jakemalachowski <jake@render.com>
2024-10-16 11:21:15 -0500

    Remove placeholder command

--- cmd/root.go
+++ cmd/root.go
@@ -89,8 +89,4 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringP("output", "o", "interactive", "interactive, json, or yaml")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
